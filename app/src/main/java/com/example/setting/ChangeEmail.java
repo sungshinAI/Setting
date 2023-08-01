b6a780a3d3fa22719b82173346dc951accbdab0f
Moon <mseongwon@naver.com>
2023-08-01 13:04:54 +0900

    Setting 변경

---
+++ app/src/main/java/com/example/setting/ChangeEmail.java
@@ -0,0 +1,14 @@
+package com.example.setting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_email);
+    }
+}
